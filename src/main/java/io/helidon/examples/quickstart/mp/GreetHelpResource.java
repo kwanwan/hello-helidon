376017fe7bfe906a22101c8705d85a806beeca0e
Sid Joshi <sid.joshi@oracle.com>
2022-03-10 13:34:34 +0000

    Create GreetHelpResource.java

---
+++ src/main/java/io/helidon/examples/quickstart/mp/GreetHelpResource.java
@@ -0,0 +1,26 @@
+package mp.quickstart;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.eclipse.microprofile.metrics.annotation.Counted;
+
+@ApplicationScoped
+@Path("/help")
+public class GreetHelpResource {
+
+    Logger LOGGER = Logger.getLogger(GreetHelpResource.class.getName());
+
+    @GET
+    @Path("/allGreetings")
+    @Counted(name = "helpCalled", description = "How many time help was called")
+    public String getAllGreetings(){
+        LOGGER.info("Help requested!");
+        return Arrays.toString(List.of("Hello","Привет","Hola","Hallo","Ciao","Nǐ hǎo", "Marhaba","Olá").toArray());
+    }
+}
