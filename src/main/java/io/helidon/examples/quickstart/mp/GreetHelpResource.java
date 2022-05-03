867e29aecd34ca7ec25149af521120a03069cbf4
Sid Joshi <sid.joshi@oracle.com>
2022-05-03 14:29:00 +0000

    Update GreetHelpResource.java

--- src/main/java/io/helidon/examples/quickstart/mp/GreetHelpResource.java
+++ src/main/java/io/helidon/examples/quickstart/mp/GreetHelpResource.java
@@ -21,6 +21,6 @@ public class GreetHelpResource {
     @Counted(name = "helpCalled", description = "How many time help was called")
     public String getAllGreetings(){
         LOGGER.info("Help requested!");
-        return Arrays.toString(List.of("Hello and Welcome DevOps Demo for Partner Forum","Привет","Hola","Hallo","Ciao","Nǐ hǎo", "Marhaba","Olá").toArray());
+        return Arrays.toString(List.of("Hello and Welcome DevOps Demo ","Привет","Hola","Hallo","Ciao","Nǐ hǎo", "Marhaba","Olá").toArray());
     }
 }
