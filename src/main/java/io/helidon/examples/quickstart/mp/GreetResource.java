456051c5c85aa03a5a20e1e2a0f2d672cd0f3d90
Kwan Wan <kwan.wan@oracle.com>
2023-02-18 07:51:58 +0000

    Update GreetResource.java

--- src/main/java/io/helidon/examples/quickstart/mp/GreetResource.java
+++ src/main/java/io/helidon/examples/quickstart/mp/GreetResource.java
@@ -68,7 +68,7 @@ public class GreetResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public JsonObject getDefaultMessage() {
-        return createResponse("World 2");
+        return createResponse("World!");
     }
 
     /**
